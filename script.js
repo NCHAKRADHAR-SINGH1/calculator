1763d338fba16e893d576466b94464d309c56d1a
NCHAKRADHAR-SINGH1 <chakri.august2006@gmail.com>
2025-08-22 15:27:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let display = document.getElementById("display");
+let currentInput = "";
+
+function appendValue(value) {
+    currentInput += value;
+    display.innerText = currentInput;
+}
+
+function clearDisplay() {
+    currentInput = "";
+    display.innerText = "";
+}
+
+function calculate() {
+    try {
+        let result = eval(currentInput);
+        display.innerText = result;
+        currentInput = result.toString();
+    } catch {
+        display.innerText = "Error";
+        currentInput = "";
+    }
+}
+s
